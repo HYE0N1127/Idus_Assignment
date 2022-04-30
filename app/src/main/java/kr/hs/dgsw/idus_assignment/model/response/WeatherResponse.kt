06524a4e2c0347ff1cd13a594fffcfcf8041c2b0
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-04-30 16:11:53 +0900

    ADD : Constatns, Api response Data Class

--- app/src/main/java/kr/hs/dgsw/idus_assignment/model/response/WeatherResponse.kt
+++ app/src/main/java/kr/hs/dgsw/idus_assignment/model/response/WeatherResponse.kt
@@ -1,8 +1,16 @@
 package kr.hs.dgsw.idus_assignment.model.response
 
+import com.google.gson.annotations.SerializedName
+
 data class WeatherResponse(
-    val weatherStateName : String,      // 날씨 요약 (ex. 맑음, 흐림... etc)
-    val weatherStateAbbr : String,      // 아이콘 이미지 (URL 형태로 날아옴)
-    val temperature : Int,      // 온도
-    val humidity : Float        // 습도
+    @SerializedName("weather_state_name")
+    val weatherStateName: String,      // 날씨 요약 (ex. 맑음, 흐림... etc)
+
+    @SerializedName("weather_state_abbr")
+    val weatherIcon: String,       // 아이콘 이미지 (URL 형태로 날아옴)
+
+    @SerializedName("the_temp")
+    val temperature: Int,      // 온도
+
+    val humidity: Float        // 습도
 )
---
+++ app/src/main/java/kr/hs/dgsw/idus_assignment/util/Constants.kt
@@ -0,0 +1,5 @@
+package kr.hs.dgsw.idus_assignment.util
+
+object Constants {
+    const val BASE_URL = "https://www.metaweather.com/api/"
+}
