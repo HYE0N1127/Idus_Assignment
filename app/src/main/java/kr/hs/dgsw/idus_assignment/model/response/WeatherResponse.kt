ccde97ab5839295f453b80578b2f9dd6ecee575d
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-04-30 18:50:56 +0900

    ADD : Get weather api by woeid

--- app/src/main/java/kr/hs/dgsw/idus_assignment/model/response/WeatherResponse.kt
+++ app/src/main/java/kr/hs/dgsw/idus_assignment/model/response/WeatherResponse.kt
@@ -5,12 +5,9 @@ import com.google.gson.annotations.SerializedName
 data class WeatherResponse(
     @SerializedName("weather_state_name")
     val weatherStateName: String,      // 날씨 요약 (ex. 맑음, 흐림... etc)
-
     @SerializedName("weather_state_abbr")
     val weatherIcon: String,       // 아이콘 이미지 (URL 형태로 날아옴)
-
     @SerializedName("the_temp")
     val temperature: Int,      // 온도
-
-    val humidity: Float        // 습도
+    val humidity: Float,        // 습도
 )
--- app/src/main/java/kr/hs/dgsw/idus_assignment/model/service/WeatherService.kt
+++ app/src/main/java/kr/hs/dgsw/idus_assignment/model/service/WeatherService.kt
@@ -1,7 +1,9 @@
 package kr.hs.dgsw.idus_assignment.model.service
 
+import kr.hs.dgsw.idus_assignment.model.response.SearchResponse
 import kr.hs.dgsw.idus_assignment.model.response.WeatherResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface WeatherService {
@@ -9,5 +11,10 @@ interface WeatherService {
     @GET("location/search/")
     suspend fun getWeatherInfoByName(
         @Query("query") query : String
-    ) : WeatherResponse
+    ) : List<SearchResponse>
+
+    @GET("location/{woeid}")
+    suspend fun getWeatherInfo(
+        @Path("woeid") woeid: String): WeatherResponse
+    )
 }
