5e0825dd51afe4021f129c41ab3f9490f844bfd5
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-05-02 15:35:26 +0900

    Fix : 동기적 코드구조에서 비동기적 코드구조로 변환

--- app/src/main/java/kr/hs/dgsw/idus_assignment/viewmodel/MainViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/idus_assignment/viewmodel/MainViewModel.kt
@@ -4,19 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kr.hs.dgsw.idus_assignment.model.data.WeatherInfo
 import kr.hs.dgsw.idus_assignment.repository.WeatherRepository
 
 class MainViewModel() : ViewModel() {
     private val repository: WeatherRepository = WeatherRepository()
-    val query : String = "Se"
+    val query: String = "Se"
 
     private val _itemList = MutableLiveData<List<WeatherInfo>>()
-    val itemList : LiveData<List<WeatherInfo>> get() = _itemList
+    val itemList: LiveData<List<WeatherInfo>> get() = _itemList
 
     private val _isLoading = MutableLiveData(false)
-    val isLoading : LiveData<Boolean> = _isLoading
+    val isLoading: LiveData<Boolean> = _isLoading
 
     init {
         searchWeatherData(query)
@@ -27,8 +28,14 @@ class MainViewModel() : ViewModel() {
 
         val response = repository.getWoeid(searchQuery)
         val list = response.map {
-            val weather = repository.getWeatherData(it.woeid.toInt())
-            WeatherInfo(it.title, weather.consolidated_weather[0], weather.consolidated_weather[1])
+            async {
+                val weather = repository.getWeatherData(it.woeid.toInt())
+                WeatherInfo(it.title,
+                    weather.consolidated_weather[0],
+                    weather.consolidated_weather[1])
+            }
+        }.map {
+            it.await()
         }
         _itemList.postValue(list)
         _isLoading.postValue(false)
